d116a042093d98b9a533cb11546573b2e5a597fd
Thiago Barbosa <thiagfb@hotmail.com>
2024-01-23 11:44:07 -0300

    Finalizando a primeira versão

---
+++ MoneyExtension/MoneyExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace MoneyExtension;
+
+public static class Money
+{
+    public static int ToCents(this decimal amount)
+    {
+        if (amount < 0)
+        {
+            return 0;
+        }
+
+        var text = amount
+            .ToString("N2")
+            .Replace(",", "")
+            .Replace(".", "");
+
+        if (String.IsNullOrEmpty(text))
+            return 0;
+
+        int.TryParse(text, out var result);
+
+        return result;
+    }
+}
